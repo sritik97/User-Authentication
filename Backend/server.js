33919f27e50231d57487267be7b19993a98744c3
sritik97 <ritiksingh9708@gmail.com>
2026-04-28 14:58:46 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -14,8 +14,10 @@ app.use(express.json())
 app.use(cors({
   origin: [
     "http://localhost:5173",
-    "https://user-authentication-indol-three.vercel.app/"
+    "http://localhost:3000",
+    "https://user-authentication-git-main-sritik97s-projects.vercel.app/"
   ],
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   credentials: true
 }));
 
